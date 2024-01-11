5431c3ab8ed38d77622580545c28d1eff4107580
Asım Tahir <AsimTahir@protonmail.com>
2024-01-12 00:53:14 +0300

    refactor: add `PYTHONENV` and `BASE_LOG_DIR` const from env var

--- koyunkirpan/constants.py
+++ koyunkirpan/constants.py
@@ -1,14 +1,38 @@
-from os import environ
+import os
+from pathlib import Path
 from distutils.util import strtobool
 import logging
-from typing import TYPE_CHECKING
-
-import koyunkirpan.logger
+from typing import Literal
 
 logger = logging.getLogger('koyunkirpan')
 
+NO_REPLY: bool
+PYTHONENV: Literal['development', 'production']
+BASE_LOG_DIR: str
+"""
+Base log directory
+"""
+
 try:
-  NO_REPLY: bool = bool(strtobool(environ.get('NO_REPLY', default='True')))
+  NO_REPLY = bool(strtobool(os.environ.get('NO_REPLY', default='True')))
+  PYTHONENV = os.environ.get('PYTHONENV', default='development')
+
+  if PYTHONENV == 'development':
+    path = Path('logs')
+    path.mkdir(parents=True, exist_ok=True)
+
+    BASE_LOG_DIR = os.environ.get('BASE_LOG_DIR', path.resolve().as_posix())
+  elif PYTHONENV == 'production':
+    path = Path('/var/log/koyunkirpan')
+    path.mkdir(parents=True, exist_ok=True)
+    
+    BASE_LOG_DIR = os.environ.get('BASE_LOG_DIR', path.resolve().as_posix())
 except Exception:
-  NO_REPLY: bool = True
-  logger.exception('\nError occured while parse to `NO_REPLY` enviroment variable\n')
+  NO_REPLY = True
+  PYTHONENV = 'development'
+
+  path = Path('logs')
+  path.mkdir(parents=True, exist_ok=True)
+  BASE_LOG_DIR = os.environ.get('BASE_LOG_DIR', path.resolve().as_posix())
+
+  logger.exception('Error occured while parse enviroment variables\n')
