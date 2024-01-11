742b16accc354ee13dc2bea47be527b92bf883f7
Asım Tahir <AsimTahir@protonmail.com>
2024-01-12 00:54:35 +0300

    refactor: use `BASE_LOG_DIR` const in logger

--- koyunkirpan/logger.py
+++ koyunkirpan/logger.py
@@ -1,7 +1,10 @@
 import sys
 import logging
 from datetime import datetime
+from koyunkirpan.constants import BASE_LOG_DIR
+# import logging.config
 
+# logging.config.fileConfig("logger.ini")
 logger = logging.getLogger('koyunkirpan')
 
 formatter = logging.Formatter(
@@ -22,7 +25,7 @@ c_handler.setLevel(logging.INFO)
 c_handler.setFormatter(formatter)
 
 # Create error file Handler with ERROR level
-f_err_handler = logging.FileHandler(filename=f'logs/error-{formatted_today}.log')
+f_err_handler = logging.FileHandler(filename=f'{BASE_LOG_DIR}/error-{formatted_today}.log')
 """
 File handler with ERROR level powered by time rotate 
 """
@@ -31,7 +34,7 @@ f_err_handler.setLevel(logging.WARNING)
 f_err_handler.setFormatter(formatter)
 
 # Create runtime file handler with INFO level
-f_runtime_handler = logging.FileHandler(filename=f'logs/runtime-{formatted_today}.log')
+f_runtime_handler = logging.FileHandler(filename=f'{BASE_LOG_DIR}/runtime-{formatted_today}.log')
 """
 File handler for runtime with INFO level powered by time rotate 
 """
@@ -40,7 +43,7 @@ f_runtime_handler.setLevel(logging.INFO)
 f_runtime_handler.setFormatter(formatter)
 
 # Create debug file handler with INFO level
-f_debug_handler = logging.FileHandler(filename=f'logs/debug-{formatted_today}.log')
+f_debug_handler = logging.FileHandler(filename=f'{BASE_LOG_DIR}/debug-{formatted_today}.log')
 """
 File handler for debugging with DEBUG level powered by time rotate 
 """
@@ -52,3 +55,5 @@ logger.addHandler(c_handler)
 logger.addHandler(f_err_handler)
 logger.addHandler(f_runtime_handler)
 logger.addHandler(f_debug_handler)
+
+logger.info(f'Logger started at "{BASE_LOG_DIR}" directory')
